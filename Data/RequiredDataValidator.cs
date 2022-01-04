625294c56bbbdb6f0746be8201551b92eb7f3711
Hetal Denish Patel <96887937+HetalDPatel@users.noreply.github.com>
2022-01-04 18:03:16 -0500

    Add files via upload

---
+++ Data/RequiredDataValidator.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace StudentsDetails.Data
+{
+    public class RequiredDataValidator : ValidationAttribute
+    {
+
+        public int min = 2;
+        public int max = 16;
+        public Boolean loop = true;
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string name = value.ToString();
+            if (name.Length > 0)
+            {
+                foreach (char ch in name)
+                {
+                    loop = false;
+                    if (!loop)
+                    {
+                        if ((ch > 65 && ch < 90) || (ch > 97 && ch < 122) || ch == 44)
+                        {
+                            loop = true;
+                        }
+                    }
+                }
+                if (name.Length >= min && name.Length <= max)
+                {
+                    return null;
+                }
+
+            }
+            return new ValidationResult(ErrorMessage, new[] { validationContext.MemberName });
+        }
+
+
+
+    }
+}
+
---
+++ Data/Student.cs
@@ -0,0 +1,55 @@
+﻿///***********************
+///Model Class For the App
+///
+/// Error: To be resolved
+/// 1.) StudentId- it takes duplicate value- refer to the StudentIdValidator Class
+/// 
+/// 
+/// </summary>
+///***********************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentsDetails.Data
+{
+    public class Student
+    {
+      
+
+        public int StudentId { get; set; }
+        public static int Id { get; set; }
+
+        public Student()
+        {
+
+            StudentId = (Id++) - 1;
+
+        }
+
+        [Required]
+        [RequiredDataValidator(ErrorMessage = "FirstName should be 2-16 character long and should be string")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [RequiredDataValidator(ErrorMessage = "Lastname should be 2-16 character long and should be string")]
+        public string LastName { get; set; }
+
+        [Required]
+        [RequiredDataValidator(ErrorMessage = "School Name should be 2-16 character long and should be string")]
+        public string SchoolName { get; set; }
+
+        [Required]
+        [RequiredDataValidator(ErrorMessage = "City Name should be 2-16 character long and should be string")]
+        public string City { get; set; }
+        
+        [Required]
+        [RequiredDataValidator(ErrorMessage = "Province should be 2-16 character long and should be string")]
+        public string Province { get; set; }
+
+      
+    }
+}
